e6bd171d7ed0c04c1c020941534caddfe5057109
ukstv <sergey@ukstv.me>
2021-05-07 17:13:24 +0300

    chore: Use dids

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
-import type { DID } from 'dids'
+import { DID } from 'dids'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import KeyDidResolver from 'key-did-resolver'
+import { Resolver } from 'did-resolver'
 
 import { createCeramic } from './ceramic'
 import { createIDX } from './idx'
@@ -14,7 +17,14 @@ const ceramicPromise = createCeramic()
 
 const authenticate = async (): Promise<string> => {
   const [ceramic, provider] = await Promise.all([ceramicPromise, getProvider()])
-  await ceramic.setDIDProvider(provider)
+  const keyDidResolver = KeyDidResolver.getResolver()
+  const threeIdResolver = ThreeIdResolver.getResolver(ceramic)
+  const resolver = new Resolver({
+    ...threeIdResolver,
+    ...keyDidResolver,
+  })
+  const did = new DID({ provider: provider, resolver: resolver })
+  await ceramic.setDID(did)
   const idx = createIDX(ceramic)
   window.did = ceramic.did
   return idx.id
