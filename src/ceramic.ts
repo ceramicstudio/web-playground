04a43c152d8a018cd7349c6f258edd3a22688919
Joel Thorstensson <joel@oed.earth>
2021-05-27 11:15:01 +0100

    chore: add streamtypes to window

--- src/ceramic.ts
+++ src/ceramic.ts
@@ -1,14 +1,19 @@
 import type { CeramicApi } from '@ceramicnetwork/common'
 import Ceramic from '@ceramicnetwork/http-client'
+import { Caip10Link } from '@ceramicnetwork/stream-caip10-link'
+import { TileDocument } from '@ceramicnetwork/stream-tile'
 
 declare global {
   interface Window {
     ceramic?: CeramicApi
+    [index: string]: any
   }
 }
 
 export async function createCeramic(): Promise<CeramicApi> {
   const ceramic = new Ceramic('https://ceramic-clay.3boxlabs.com')
   window.ceramic = ceramic
+  window.TileDocument = TileDocument
+  window.Caip10Link = Caip10Link
   return Promise.resolve(ceramic as CeramicApi)
 }
