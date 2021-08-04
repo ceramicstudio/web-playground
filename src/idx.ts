8688ec8da2d3e131c6a879386c02646f02876cd7
Elizabeth Murray <elizabeth@3box.io>
2021-08-04 12:58:00 -0400

    remove idx-tools functions

--- src/idx.ts
+++ src/idx.ts
@@ -1,6 +1,7 @@
 import type { CeramicApi } from '@ceramicnetwork/common'
 import { IDX } from '@ceramicstudio/idx'
-import { createTile, createDefinition } from '@ceramicstudio/idx-tools'
+
+import { schemas } from "@ceramicstudio/idx-constants"
 
 declare global {
   interface Window {
@@ -10,10 +11,12 @@ declare global {
 
 export function createIDX(ceramic: CeramicApi): IDX {
   const idx = new IDX({ ceramic })
-  window.createTile = createTile
-  window.createDefinition = createDefinition
+
+  window.definition = schemas.Definition
+
   window.exampleDID = 'did:key:z6MkuEd4fm7qNq8hkmWFM1NLVBAXa4t2GcNDdmVzBrRm2DNm'
   window.exampleDID2 = 'did:3:kjzl6cwe1jw146g16yd2w2dhcirw4zhevar48divve67jx3fq5ztxsx12aimr5y'
+  
   window.idx = idx
   return idx
 }
