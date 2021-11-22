8775bc7b6972ba357afe29253d60fc41b5bc346f
Elizabeth Murray <elizabeth@3box.io>
2021-11-22 18:08:02 -0500

    resolve issues with production build & create RELEASE.md 'checklist'

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: {
     app: './src/app.ts',
@@ -22,10 +24,17 @@ module.exports = {
       https: require.resolve('https-browserify'),
       http: require.resolve('http-browserify'),
       os: require.resolve('os-browserify/browser'),
+      buffer: require.resolve('buffer'),
       assert: false,
       url: false,
     },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ],
   output: {
     filename: '[name].js',
   },
